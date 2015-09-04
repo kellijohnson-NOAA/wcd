4542232a2e746e3e4e2c7d00aa5fb2de02f28f83
kellijohnson <kellifayejohnson@gmail.com>
2015-09-04 07:14:11 -0700

    change comment style

--- wcd_master.R
+++ wcd_master.R
@@ -8,21 +8,21 @@
 ###############################################################################
 ###############################################################################
 
-###############################################################################
-#### Set variable inputs
-###############################################################################
+#' Set variable inputs
 my.dir <- "c:/wcd"
 if (!file.exists(my.dir)) my.dir <- "d:/wcd"
 
-###############################################################################
-#### Create folder structures
-###############################################################################
-library(gamm4)
+#' Create folder structures
 setwd(my.dir)
 
+#' Directories
 dir.data <- file.path(my.dir, "data")
 dir.results <- file.path(my.dir, "results")
 
 #' Files
 file.surveyspp <- "2009To2012CatchForSeminar.xlsx"
 file.surveydata <- "FisheryIndices2015_Sablefish_V7.xlsx"
+#' Inputs
+#' Packages
+#' Set the CRAN mirror and install packages that are not currently installed
+options("repos" = "http://R-Forge.R-project.org")
