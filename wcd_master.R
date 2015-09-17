34ce84d695f512157c3a4dcc2cbf1e9e5bd3ecdc
kellijohnson <kellifayejohnson@gmail.com>
2015-09-17 11:55:20 -0700

    Update needed packages

--- wcd_master.R
+++ wcd_master.R
@@ -32,7 +32,8 @@ my.spp <- c("sablefish",
 #' Packages
 #' Set the CRAN mirror and install packages that are not currently installed
 options("repos" = "http://R-Forge.R-project.org")
-pckg.need <- c("censReg", "devtools", "ggmap", "knitr", "nwfscDeltaGLM", "xlsx")
+pckg.need <- c("censReg", "devtools", "ggmap", "knitr", "nwfscDeltaGLM",
+  "plm", "xlsx")
 for (pckg in pckg.need[!pckg.need %in% installed.packages()[, "Package"]]){
   if (pckg %in% c("nwfscDeltaGLM")) {
     devtools::install_github("nwfsc-assess/nwfscDeltaGLM", ref = "1.0.0")
