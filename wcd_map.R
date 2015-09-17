49bbd4d9979fde0d1e53471d128febf07dae917c
kellijohnson <kellifayejohnson@gmail.com>
2015-09-17 11:52:02 -0700

    Changes to map, don't really know what for

--- wcd_map.R
+++ wcd_map.R
@@ -19,12 +19,14 @@ dir.create("maps", showWarnings = FALSE)
 #' * Pacific Ocean Perch (POP) - rebuilt but included b/c high bycatch
 #'
 #+ raw_survey, echo = FALSE, warning = FALSE, include = FALSE, cache = TRUE, message = FALSE>>=
-my.spp <- c("bocaccio", "canary.roc", "darkblotch", "Pacific.oc",
-  "sablefish", "widow.rock", "yelloweye")
-my.sppgood <- sapply(strsplit(levels(data.plot$species), "\\."), "[", 1)
-my.sppgood <- gsub("Pacific", "POP", my.sppgood)
-data.need <- data.bio[, which(colnames(data.bio) %in%
-  c("X", "Y", "Survey_Cyc", my.spp))]
+my.spp <- c("bocaccio", "canary", "darkblotch", "POP",
+  "sablefish", "widow", "yelloweye")
+my.spp <- c("darkblotch", "POP","sablefish")
+colnames(data.bio)[which(colnames(data.bio) == "Pacific.oc")] <- "POP"
+sppgood <- sapply(strsplit(colnames(data.bio), "\\."), "[", 1)
+data.need <- data.bio[, c(which(colnames(data.bio) %in% c("X", "Y", "Survey_Cyc")),
+  which(sppgood %in% my.spp == TRUE))]
+
 data.plot <- reshape(data.need, idvar = c("X", "Y", "Survey_Cyc"),
   varying = list(4:NCOL(data.need)), v.name = "weight", direction = "long")
 rownames(data.plot) <- NULL
@@ -34,15 +36,19 @@ data.plot$year <- as.numeric(sapply(strsplit(as.character(data.plot$year),
 data.plot <- subset(data.plot, weight > 0)
 data.plot$species <- factor(data.plot$time, levels = 1:length(my.spp),
   labels = colnames(data.need)[-c(1:3)])
-data.plot$species <- factor(data.plot$species, levels = levels(data.plot$species),
-  labels = my.sppgood)
 
 #+plot_raw_survey, echo = FALSE, warning = FALSE, message = FALSE
 p <- ggmap(get_map("Redding, California", maptype = "terrain", zoom = 5, source = "google"))
 p +
   facet_grid(species ~ year) +
   geom_point(data = data.plot, aes(x = X, y = Y, size = weight / 10), alpha = 0.25) +
-  theme(text = element_text(size = 5)) +
+  theme(text = element_text(size = 12)) +
+  scale_alpha(guide = FALSE) + scale_size(guide = FALSE) + xlab("") + ylab("")
+
+p +
+  facet_grid(species ~ year) +
+  geom_point(data = subset(data.plot, species == "sablefish"), aes(x = X, y = Y, size = weight / 10), alpha = 0.25) +
+  theme(text = element_text(size = 12)) +
   scale_alpha(guide = FALSE) + scale_size(guide = FALSE) + xlab("") + ylab("")
 
 #'Raw landed weight of sablefish and petrale from the Northwest Fisheries
