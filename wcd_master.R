ad40eface6aaa00d7e8178b2286f186aca6e6a60
kellijohnson <kellifayejohnson@gmail.com>
2015-09-24 09:22:39 -0700

    "Add raster to the list of needed packages"

--- wcd_master.R
+++ wcd_master.R
@@ -38,7 +38,7 @@ height <- width
 #' Set the CRAN mirror and install packages that are not currently installed
 options("repos" = "http://R-Forge.R-project.org")
 pckg.need <- c("devtools", "gamlss", "ggmap", "knitr", "nwfscDeltaGLM",
-  "xlsx")
+  "raster", "xlsx")
 for (pckg in pckg.need[!pckg.need %in% installed.packages()[, "Package"]]){
   if (pckg %in% c("nwfscDeltaGLM")) {
     devtools::install_github("nwfsc-assess/nwfscDeltaGLM", ref = "1.0.0")
