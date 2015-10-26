ff7259312958d316a62350ced6abd04eb7bf67c3
kellijohnson <kellifayejohnson@gmail.com>
2015-10-25 17:57:28 -0700

    Add package and code for plots

--- wcd_map.R
+++ wcd_map.R
@@ -2,24 +2,15 @@
 #' Title: Spatial data regarding US West Coast groundfish fisheries
 #' Author: Kelli Faye Johnson
 #' ----
-#+ setup, include = FALSE
-# set global chunk options
-opts_chunk$set(fig.path = file.path(dir.results, "figure"),
-  fig.align = "center", fig.show = "hold")
-options(formatR.arrow = TRUE, width = 90)
 
-#' Goal - How the fishery responds to multiple socio-ecological drivers?
-#'
-#' Species -
-#' * cowcod - not included b/c minimal to zero bycatch
-#' * Pacific Ocean Perch (POP) - rebuilt but included b/c high bycatch
-#'
-#'Raw landed weight of sablefish and petrale from the Northwest Fisheries
-#'Science Center Shelf-Slope Survey. The size of the circle is proportional to the
-#'landed weight, with circles being semi-transparent to show tows within close
-#'proximity of each other.
+# Variable inputs
+small <- 0.10
 
-#+ raw_survey, echo = FALSE, warning = FALSE, include = FALSE, cache = TRUE, message = FALSE>>=
+#### Data
+getstrata <- eval(parse(text = gsub(",$", "",
+  grep("SLat", readLines("wcd_survey.R"), value = TRUE))))
+data.yrin <- read.csv(file.path(dir.results, file.index))
+
 data.plot <- data.srvy
 colnames(data.plot)[grep("Pacific\\.oc", colnames(data.plot))] <- "POP"
 colnames(data.plot)[grep("LAT", colnames(data.plot))] <- "Y"
@@ -42,7 +33,7 @@ data.plot$year <- as.numeric(sapply(strsplit(as.character(data.plot$year),
   " "), "[", 2))
 data.plot <- subset(data.plot, weight > 0)
 
-#+plot_raw_survey, echo = FALSE, warning = FALSE, message = FALSE
+#### Maps
 us <- getData("GADM", country = "USA", level = 1)
 canada <- getData("GADM", country = "CAN",level = 1)
 
@@ -53,11 +44,8 @@ us.bbox <- bbox(us.states)
 xlim <- c(min(us.bbox[1, 1]) * 1.02, max(us.bbox[1, 2]))
 ylim <- c(min(us.bbox[2, 1]) * 0.98, max(us.bbox[2, 2]))
 
-# p <- ggmap(get_map(location = "Redding, California", source = "google",
-#   maptype = "roadmap", zoom = 5, messaging = FALSE, color = "color"))
-
-p <- ggplot() +
-  theme_bw() +
+#### ggplot theme
+theme <-   theme_bw() +
   theme(plot.background = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -66,27 +54,31 @@ p <- ggplot() +
         legend.key = element_rect(colour = "white"),
         legend.title = element_text(size = 7, face = "bold"),
         legend.text = element_text(size = 7, face = "bold")
-  ) +
+  )
+
+p <- ggplot() +
+  theme +
   xlim(xlim) + ylim(ylim) + xlab("") + ylab("")
 
-small <- 0.10
-p +
+#### Start individual maps
+###############################################################################
+pp <- p +
   geom_point(data = data.plot,
-    aes(x = X, y = Y, size = weight / 10), alpha = 0.25) +
+    aes(x = X, y = Y, size = weight / 10), pch = 1, alpha = 0.5) +
   theme(legend.position = "none",
         axis.text = element_text(size = 6)) +
   geom_path(data = us.states, aes(x = long, y = lat, group = group), size = small) +
   geom_path(data = ca.provinces, aes(x = long, y = lat, group = group), size = small) +
   coord_map() +
-  facet_grid(species ~ year)
-ggsave(filename = "abundance.png",
-  plot = last_plot(), path = dir.results, scale = 1,
-  width = par("din")[1], height = par("din")[2], units = "in",
+  facet_wrap( ~ species) +
+  theme(strip.text = element_text(vjust = -1.25),
+        panel.margin = unit(c(-0.5, 0, -0.5, 0), "lines"))
+ggsave(filename = "abundance.png", pp, path = dir.results,
   dpi = 300, limitsize = TRUE)
+###############################################################################
 
-getstrata <- eval(parse(text = gsub(",$", "",
-  grep("SLat", readLines("wcd_survey.R"), value = TRUE))))
-p +
+###############################################################################
+pp <- p +
   theme(legend.position = "none",
         axis.text = element_text(size = 6)) +
   geom_path(data = us.states, aes(x = long, y = lat, group = group), size = small) +
@@ -97,12 +89,32 @@ p +
   annotate("text", x = -122.9, y = 42.833, label = "Cape Blanco", size = 3) +
   annotate("text", x = -125.5, y = getstrata + 0.5,
            label = paste0("(", letters[1:9], ")"), size = 3)
-ggsave(filename = "strata.png",
-  plot = last_plot(), path = dir.results, scale = 1,
+ggsave(filename = "strata.png", pp, path = dir.results, scale = 1,
   width = par("din")[1], height = par("din")[2], units = "in",
   dpi = 300, limitsize = TRUE)
 dev.off()
+###############################################################################
 
-#+endoffile, echo = FALSE
+###############################################################################
+indexlong <- reshape(data.yrin,
+  direction = "long", varying = my.spp, v.name = "weight",
+  timevar = "species", times = my.spp)
+rownames(indexlong) <- NULL
+indexlong$species <- sapply(strsplit(indexlong$species, "\\."), "[", 1)
+indexlong$species[indexlong$species == "Pacific"] <- "POP"
+# Print correct levels for letters
+levels(data.cost$portgrp)[c(9, 1, 7, 3, 2, 4, 5, 8, 6)]
+
+pp <- ggplot() + theme +
+  geom_line(data = subset(indexlong, year %in% 2009:2013),
+    aes(x = year, y = weight)) +
+  facet_grid(species ~ strat, scales = "free") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.2))
+ggsave(filename = "indexlines.png", pp, path = dir.results, scale = 1,
+  dpi = 300, limitsize = TRUE)
+dev.off()
+###############################################################################
+
+#### endoffile
 setwd(dir.old)
 rm(dir.old)
--- wcd_master.R
+++ wcd_master.R
@@ -42,7 +42,8 @@ height <- width
 #' Packages
 #' Set the CRAN mirror and install packages that are not currently installed
 options("repos" = "http://R-Forge.R-project.org")
-pckg.need <- c("devtools", "gamlss", "ggmap", "knitr", "nwfscDeltaGLM", "raster")
+pckg.need <- c("devtools", "gamlss", "ggmap", "ggplot2", "grid",
+  "knitr", "nwfscDeltaGLM", "raster")
 for (pckg in pckg.need[!pckg.need %in% installed.packages()[, "Package"]]){
   if (pckg %in% c("nwfscDeltaGLM")) {
     devtools::install_github("nwfsc-assess/nwfscDeltaGLM", ref = "1.0.0")
