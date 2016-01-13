f7bc86de8756382877a17bd7969de7380f291d29
kellijohnson <kellifayejohnson@gmail.com>
2016-01-13 07:24:01 -0800

    Script to create dendrogram

--- wcd_cluster.R
+++ wcd_cluster.R
@@ -1,15 +1,30 @@
+#' ----
+#' Title: Partitioning analysis of catches by gear type
+#' Author: Kelli Faye Johnson
+#' ----
 
-setwd("c:/wcd/data")
-catch <- read.csv("catchbyyearfleet.csv")
+#' Read in the catch data as provided for the stock assessment
+#' Catches are grouped according to gear that was used while fishing
+#' not permit type held while fishing.
+catch <- read.csv(file.path(dir.data, "catchbyyearfleet.csv"))
 catch <- aggregate(catch ~ year + fleet, data = catch, sum)
-catch <- (reshape(catch, direction = "wide", timevar = "fleet", idvar = "year"))
+catch <- reshape(catch, direction = "wide", timevar = "fleet", idvar = "year")
 
-test <- data.frame("year" = catch[, 1], 
+#' Change absolute catches into proportion of the total catch for each gear
+props <- data.frame("year" = catch[, 1],
   prop.table(as.matrix(catch[, -1]), margin = 1))
-test <- subset(test, year >= 1982)
-res <- dist(test)
+props <- subset(props, year >= 1982)
 
+#' Calculate the distance between each observation
+res <- dist(props)
 mod <- hclust(res)
-plot(mod, labels = test$year)
-rect.hclust(mod, 6)
 
+#' Create the figure used in the publication
+png(file.path(dir.results, "dendrogram.png"),
+  res = resolution, width = width, height = height / 2)
+par(mar = c(2, 2.5, 0.25, 0.25), las = 1)
+plot(mod, labels = props$year,
+  xlab = "", ylab = "", main = "")
+mtext(side = 1, line = 0.5, "year")
+rect.hclust(mod, k = 6, border = 1)
+dev.off()
